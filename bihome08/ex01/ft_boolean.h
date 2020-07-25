48fae015dee3c8a83fd09a1723d47001bc5b3353
stas <andreeff.stas2014@gmail.com>
2020-07-25 08:15:29 +0300

    00 01 home try

---
+++ bihome08/ex01/ft_boolean.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+# define t_bool int
+# define EVEN(number) (number % 2) ? (0) : (1)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
