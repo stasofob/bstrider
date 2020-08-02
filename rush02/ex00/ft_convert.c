519535e73e59c78dd70f8c5954742406b82c1a62
Bibi Strider <bstrider@at-m7.msk.21-school.ru>
2020-08-02 19:37:56 +0300

    исправляем ошибки компиляции

--- rush02/ex00/ft_convert.c
+++ rush02/ex00/ft_convert.c
@@ -17,48 +17,50 @@ char *generate_num(char ch, int rank)
 		ret[cnt] = '\0';
 	}
 	return (ret);
-}///////////////////////
+}
+
 void print_list(t_list *dict)
 {
 	t_list *tmp = dict;
 	while (tmp)
 	{
-		ft_putstr("key: ");
-		ft_putstr(tmp->key);
-		ft_putstr(" , data: ");
 		ft_putstr(tmp->data);
-		ft_putstr("\n");
+		if (tmp->next)
+			ft_putstr(" ");
 		tmp = tmp->next;
 	}
-}//////////////////////////
+	ft_putstr("\n");
+}
+
 int ft_convert(t_list *dict, char *str, int size)
 {
-	int	i;
+	int		i;
 	char	*key;
-	int success;
+	int		success;
 	t_list	*number;
+	int		flag_null;
+
 	i = size;
 	number = NULL;
 	success = 1;
-	int flag = 0;
-	//print_list(dict);
+	flag_null = 0;
 	while (*str != '\0')
 	{
-		if (i % 3 == 0 && flag)
+		if (i % 3 == 0 && flag_null)
 		{
 			key = generate_num('1', i);
-			ft_list_push_back(&number, key, strdup(ft_find_data(dict, key)));
-			flag = 0;
+			ft_list_push_back(&number, key, ft_strdup(ft_find_data(dict, key)));
+			flag_null = 0;
 		}
 		if (*str != '0')
 		{
-			flag = 1;
+			flag_null = 1;
 			if (i % 3 == 2)
 			{
 				if (i != size)
 				{	
-					key = generate_num('1',2);
-					ft_list_push_back(&number, key, strdup(ft_find_data(dict,key)));
+					key = generate_num('1', 2);
+					ft_list_push_back(&number, key, ft_strdup(ft_find_data(dict,key)));
 				}
 				if (*str == '1')
 				{
@@ -75,9 +77,7 @@ int ft_convert(t_list *dict, char *str, int size)
 		str++;
 		i--;
 	}
-	ft_putstr("result list\n");
 	print_list(number);
 	return (success);
 }
-
 
--- rush02/ex00/main.c
+++ rush02/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: bstrider <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 10:55:33 by bstrider          #+#    #+#             */
-/*   Updated: 2020/08/02 15:47:51 by bstrider         ###   ########.fr       */
+/*   Updated: 2020/08/02 19:36:25 by bstrider         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ int			main(int argc, char **argv)
 	{
 		
 		if (dict = ft_dict(DEFAULT_FILE, ft_file_size(DEFAULT_FILE)))
-			if (ft_convert(dict, argv[1], size_number))//ft_znach(argv[1], size_number);
+			if (ft_convert(dict, argv[1], size_number))
 				return (0);
 		ft_putstr(DICT_PARSING);
 	}
 	else if (argc == 3 && (size_number = ft_isnumber(argv[2])))
 	{
-		if (dict = ft_dict(argv[1], ft_file_size(argv[1])))
+		if ((dict = ft_dict(argv[1], ft_file_size(argv[1]))))
 			if (ft_convert(dict, argv[2], size_number))
 				return (0);	
 		ft_putstr(DICT_PARSING);
